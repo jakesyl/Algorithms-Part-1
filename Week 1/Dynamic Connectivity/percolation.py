1ed177a4c7d5027f596d4f7d29429a2f1e0330b4
Jake <jakesyl1998@gmail.com>
2014-12-25 03:19:33 +0000

    percolation

---
+++ Week 1/Dynamic Connectivity/percolation.py
@@ -0,0 +1,10 @@
+'''
+Here is some information to help you interpret the assessment report. See the Assessment Guide for more details.
+Compilation: we compile your .java files using a Java 7 compiler. Any error or warning messages are displayed and usually signify a major defect in your code.
+Style: we run checkstyle to automatically checks the style of your Java programs. Here is a list of available Checkstyle checks, which you can use to help decode any warning messages.
+Bugs: we run findbugs to check for common bug patterns in Java programs. A warning message strongly suggests a bug in your code but occasionally there are false positives. Here is a summary of bug descriptions, which you can use to help decode warning messages.
+API: we check that your code exactly matches the prescribed API (no extra methods and no missing methods). If it does not, no further tests are performed.
+Correctness: we perform a battery of unit tests to check that your code meets the specifications.
+Memory: we determine the amount of memory according to the 64-bit memory cost model from lecture.
+Timing: we measure the running time and count the number of elementary operations.
+'''
